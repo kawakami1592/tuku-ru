432337fe5aaa440a409a9df551b7b171970129c6
kawakami1592 <m.kawakami157@icloud.com>
2020-12-05 20:41:18 +0900

    tuika

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 'use strict';
 
 {
+// ハンバーガーメニュー
+  const navBtn = document.getElementById('nav-btn');
+  const hamburger = document.getElementById('hamburger')
+  navBtn.addEventListener('click', () => {
+    navBtn.classList.toggle('nav-open');
+    hamburger.classList.toggle('hamburger-close');
+    mask.classList.toggle('hidden');
+  });
+
+// モーダルウィンドウ
   const open = document.getElementById('open');
   const close = document.getElementById('close');
   const modal = document.getElementById('modal');
@@ -10,13 +20,17 @@
     modal.classList.remove('hidden');
     mask.classList.remove('hidden');
   });
-
   close.addEventListener('click', () => {
     modal.classList.add('hidden');
     mask.classList.add('hidden');
   });
 
+// マスク
   mask.addEventListener('click', () => {
-    close.click(); //closeの'click'イベントを呼んでいる
+    close.click();
+    navBtn.classList.remove('nav-open');
+    hamburger.classList.add('hamburger-close');
+
   });
+
 }
