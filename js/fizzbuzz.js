ec58b9521532505fb9a2ce7ba9f5aadfe02656bc
kawakami1592 <m.kawakami157@icloud.com>
2020-12-16 01:28:19 +0900

    fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,6 +1,20 @@
 'use strict';
 {
-  const inputNum = document.getElementById('input-fizzbuzz');
-  let Num = inputNum.value;
-  console.log(Num);
+  const inputNum = document.getElementById('form-fizzbuzz');
+
+  inputNum.addEventListener('submit',(e)=> {
+    e.preventDefault();
+    const inputNum = document.getElementById('input-fizzbuzz').value;
+    
+    if(inputNum % 15 === 0){
+      console.log('FizzBuzz');
+    }else if(inputNum % 5 === 0){
+      console.log('Buzz');
+    }else if(inputNum % 3 === 0){
+      console.log('Fizz');
+    }else{
+      console.log(inputNum);
+    }
+  })
+
 }
