76cb64be0016f73bf45c25ffbac7d4f06041146a
kawakami1592 <m.kawakami157@icloud.com>
2020-12-16 06:44:13 +0900

    fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -5,15 +5,25 @@
   inputNum.addEventListener('submit',(e)=> {
     e.preventDefault();
     const inputNum = document.getElementById('input-fizzbuzz').value;
+    const fizzbuzzAns = document.getElementById('fizzbuzz-ans')
     
     if(inputNum % 15 === 0){
       console.log('FizzBuzz');
+      // const createAns = document.createElement('')
+      fizzbuzzAns.textContent = "FizzBuzz";
+
     }else if(inputNum % 5 === 0){
       console.log('Buzz');
+      fizzbuzzAns.textContent = "Buzz";
+
     }else if(inputNum % 3 === 0){
       console.log('Fizz');
+      fizzbuzzAns.textContent = "Fizz";
+
     }else{
       console.log(inputNum);
+      fizzbuzzAns.textContent = "どちらでもないです";
+
     }
   })
 
