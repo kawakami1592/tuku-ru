285823c8eb6547a974a308ff83e8bcb3cd441e9d
kawakami1592 <m.kawakami157@icloud.com>
2020-12-26 21:07:40 +0900

    キャンバス

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,5 @@
+'use strict';
+
 {
   function draw() {
     const canvas = document.querySelector('canvas');
@@ -6,14 +8,15 @@
     }
     const ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'pink';
-    ctx.strokeStyle = '#f00';
-    ctx.lineWidth = 8;
-    // ctx.lineJoin = 'round';
-    ctx.lineJoin = 'bevel';
+    // ctx.createLinearGradient(x0, y0, x1, y1);
+    const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
+
+    g.addColorStop(0, '#f00');
+    g.addColorStop(0.3, '#0f0');
+    g.addColorStop(1, '#00f');
     
-    ctx.fillRect(50, 50, 50, 50);
-    ctx.strokeRect(50, 50, 50, 50);
+    ctx.fillStyle = g;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 
   draw();
