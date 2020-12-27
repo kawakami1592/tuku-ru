776ec4e47906921c9317a322d5d89f7ddf6e3ac4
kawakami1592 <m.kawakami157@icloud.com>
2020-12-27 23:27:41 +0900

    線

--- js/canvas.js
+++ js/canvas.js
@@ -8,15 +8,14 @@
     }
     const ctx = canvas.getContext('2d');
 
-    // ctx.createLinearGradient(x0, y0, x1, y1);
-    const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
-
-    g.addColorStop(0, '#f00');
-    g.addColorStop(0.3, '#0f0');
-    g.addColorStop(1, '#00f');
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(100, 50);
+    ctx.lineTo(200, 100);
+    // ctx.closePath();
     
-    ctx.fillStyle = g;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
+    // ctx.fill();
   }
 
   draw();
