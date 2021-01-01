abbd7981562930cc8e8a20b5bae9fb396951e79c
kawakami1592 <m.kawakami157@icloud.com>
2021-01-02 00:23:04 +0900

    アニメーション

--- js/canvas.js
+++ js/canvas.js
@@ -10,6 +10,16 @@
     }
     const ctx = canvas.getContext('2d');
 
+    const CANVAS_WIDTH = canvas.width;
+    const CANVAS_HEIGHT = canvas.height;
+    const dpr = window.devicePixelRatio || 1;
+    canvas.width = CANVAS_WIDTH * dpr;
+    canvas.height = CANVAS_HEIGHT * dpr;
+    console.log(canvas.width);
+    ctx.scale(dpr, dpr);
+    canvas.style.width = CANVAS_WIDTH + 'px';
+    canvas.style.height = CANVAS_HEIGHT + 'px';
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
@@ -18,7 +28,7 @@
     ctx.fill();
 
     ctx.beginPath();
-    ctx.ellipse(80 + Math.sin(t / 10), 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.ellipse(80 + 5*Math.sin(t / 10), 100, 8, 8, 0, 0, 2 * Math.PI);
     ctx.ellipse(120 + Math.sin(t / 30), 100, 8, 8, 0, 0, 2 * Math.PI);
     ctx.fillStyle = 'skyblue';
     ctx.fill();
