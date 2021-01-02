1e56841bf762d9bd68f84201c1746ac86c89c8ee
kawakami1592 <m.kawakami157@icloud.com>
2021-01-02 17:25:27 +0900

    パズル

--- js/canvas.js
+++ js/canvas.js
@@ -43,22 +43,33 @@
 
   (() => {
     class Puzzle {
-      constructor(canvas) {
+      constructor(canvas, level) {
         this.canvas = canvas;
+        this.level = level;
         this.ctx = this.canvas.getContext('2d');
-        //タイル縦横配置用２重配列
+
+        //タイル初期配置用２重配列
         this.tiles = [
           [0 , 1 , 2 , 3 ],
           [4 , 5 , 6 , 7 ],
           [8 , 9 , 10, 11],
           [12, 13, 14, 15],
         ];
+        //タイル移動用
+        this.UDLR = [
+          [0, -1], // up
+          [0, 1], // down
+          [-1, 0], // left
+          [1, 0], // right
+        ];
+
         this.img = document.createElement('img');
         this.img.src = 'images/15puzzle.png';
         this.img.addEventListener('load', () => {
           this.render();
         });
 
+        //タイルクリックイベント
         this.canvas.addEventListener('click', e => {
           //キャンバスの配置情報を取得
           const rect = this.canvas.getBoundingClientRect();
@@ -69,58 +80,17 @@
           console.log(col, row);
           this.swapTiles(col, row);
           this.render();
+
+          //ゲームクリアの判定
+          if (this.isComplete() === true) {
+            this.renderGameClear();
+          }
         });
+
+        this.shuffle(this.level);
       }
 
-      swapTiles(col, row) {
-        if (this.tiles[row][col] === 15) {
-          return;
-        }
-  
-        //クリックされたタイルの隣り合うタイルを調べる
-        for (let i = 0; i < 4; i++) {
-          let destCol;
-          let destRow;
-          switch (i) {
-            case 0: // up
-              destCol = col;
-              destRow = row - 1;
-              break;
-            case 1: // down
-              destCol = col;
-              destRow = row + 1;
-              break;
-            case 2: // left
-              destCol = col - 1;
-              destRow = row;
-              break;
-            case 3: // right
-              destCol = col + 1;
-              destRow = row;
-              break;
-          }
-          if (//上下左右がキャンバスからはみ出す場合はループをスキップする
-            destCol < 0 || destCol > 3 ||
-            destRow < 0 || destRow > 3
-          ) {
-            continue;
-          }
-  
-          //隣り合うタイルが15番なら分割代入で入れ替える
-          if (this.tiles[destRow][destCol] === 15) {
-            [
-              this.tiles[row][col],
-              this.tiles[destRow][destCol],
-            ] = [
-              this.tiles[destRow][destCol],
-              this.tiles[row][col],
-            ];
-            break;
-          }
-        }
-      }
-  
-      //タイル縦横配置用２重配列を回し座標を[row][col]で取得
+      //タイル初期配置用２重配列を回し座標を[row][col]で取得
       render() {
         for (let row = 0; row < 4; row++) {
           for (let col = 0; col < 4; col++) {
@@ -137,6 +107,125 @@
           col * 70, row * 70, 70, 70//再配置位置
         );
       }
+  
+      //値が枠外(0~3)ならtrueを返す
+      isOutside(destCol, destRow) {
+        return (
+          destCol < 0 || destCol > 3 || destRow < 0 || destRow > 3
+        );
+      }
+
+      //初期シャッフル
+      shuffle(n) {
+        //15番の初期値
+        let blankCol = 3;
+        let blankRow = 3;
+      
+        //シャッフル
+        for (let i = 0; i < n; i++) {
+          let destCol;
+          let destRow;
+
+          do {
+            const dir = Math.floor(Math.random() * 4);//0~3のランダムな数値
+            destCol = blankCol + this.UDLR[dir][0];
+            destRow = blankRow + this.UDLR[dir][1];
+            // switch (dir) {
+            //   case 0: // up
+            //     destCol = blankCol + UDLR[0][0]
+            //     destRow = blankRow + UDLR[0][1];
+            //     break;
+            //   case 1: // down
+            //     destCol = blankCol + UDLR[1][0]
+            //     destRow = blankRow + UDLR[1][1]
+            //     break;
+            //   case 2: // left
+            //     destCol = blankCol + UDLR[2][0]
+            //     destRow = blankRow + UDLR[2][1]
+            //     break;
+            //   case 3: // right
+            //     destCol = blankCol + UDLR[3][0]
+            //     destRow = blankRow + UDLR[3][1]
+            //     break;
+            // }
+
+          //上下左右がキャンバスからはみ出す場合は次のループへスキップする
+          } while (this.isOutside(destCol, destRow) === true);
+
+          [
+            this.tiles[blankRow][blankCol],
+            this.tiles[destRow][destCol],
+          ] = [
+            this.tiles[destRow][destCol],
+            this.tiles[blankRow][blankCol],
+          ];
+  
+          [blankCol, blankRow] = [destCol, destRow];
+        }
+      }
+
+      //タイルのクリックアクション
+      swapTiles(col, row) {
+        if (this.tiles[row][col] === 15) {
+          return;
+        }
+  
+        //クリックされたタイルの隣り合うタイルを調べる
+        for (let i = 0; i < 4; i++) {
+          const destCol = col + this.UDLR[i][0];
+          const destRow = row + this.UDLR[i][1];
+
+          // switch (i) {
+          //   case 0: // up
+          //     destCol = col;
+          //     destRow = row - 1;
+          //     break;
+          //   case 1: // down
+          //     destCol = col;
+          //     destRow = row + 1;
+          //     break;
+          //   case 2: // left
+          //     destCol = col - 1;
+          //     destRow = row;
+          //     break;
+          //   case 3: // right
+          //     destCol = col + 1;
+          //     destRow = row;
+          //     break;
+          // }
+
+          //上下左右がキャンバスからはみ出す場合は次のループへスキップする
+          if (this.isOutside(destCol, destRow) === true) {
+            continue;
+          }
+  
+          //隣り合うタイルが15番なら分割代入で入れ替える
+          if (this.tiles[destRow][destCol] === 15) {
+            [
+              this.tiles[row][col],
+              this.tiles[destRow][destCol],
+            ] = [
+              this.tiles[destRow][destCol],
+              this.tiles[row][col],
+            ];
+            break;
+          }
+        }
+      }
+
+      //クリア判定
+      isComplete() {
+        return true;
+      }
+      //クリア画面
+      renderGameClear() {
+        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.font = '28px Arial';
+        this.ctx.fillStyle = '#fff';
+        this.ctx.fillText('GAME CLEAR!!', 40, 150);
+      }
+
     }
 
 
@@ -145,7 +234,7 @@
       return;
     }
     //インスタンス生成
-    new Puzzle(canvas);
+    new Puzzle(canvas ,30);
   })();
 
 }
