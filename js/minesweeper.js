84944f3dcf2cad44f7bda4abf3b78e919a3ce471
kawakami1592 <m.kawakami157@icloud.com>
2021-01-25 00:54:38 +0900

    minesweeper

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,41 +1,28 @@
 {
   class Tiles{
-    constructor(canvas){
+    constructor(canvas,game){
       this.canvas = canvas;
+      this.game = game;
       this.ctx    = this.canvas.getContext('2d');
-
-      //タイル初期配置用２重配列
-      // this.tiles = [
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1],
-      //   [1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1]
-      // ];
-      this.tiles = [//1:タイル 2:爆弾
-        [1 ,1 ,1 ,1 ,1 ],
-        [1 ,2 ,1 ,2 ,1 ],
-        [1 ,1 ,1 ,1 ,1 ],
-        [1 ,1 ,2 ,1 ,1 ],
-        [1 ,1 ,1 ,1 ,1 ]
+      
+      this.UDLR = [
+        [0, -1], // up
+        [0, 1], // down
+        [-1, 0], // left
+        [1, 0], // right
       ];
-      this.tileRow = this.tiles.length;//行数
-      this.tileCol = this.tiles[0].length;//列数
+      this.tileRow = 10;//行数
+      this.tileCol = 10;//列数
 
-      //初期位置
-      this.x = 0;
-      this.y = 0;
+      this.tiles = [];
+      this.max = 4; 
+
+      this.getTiles(this.tileRow,this.tileCol,this.max)
+      console.log(this.tiles);
 
       this.tileSize = canvas.width / this.tileRow
       this.stage = canvas.width / this.grid;
 
-      this.data = [];
-      this.draw();
       //失敗フラグ
       this.isMissed = false;
 
@@ -47,131 +34,143 @@
         //クリック位置からタイルの座標を特定
         const col = Math.floor((e.clientX - rect.left) / this.tileSize);
         const row = Math.floor((e.clientY - rect.top) / this.tileSize);
-        console.log(col, row);
-        this.search(col, row);
 
+        if(this.isMissed){
+          return;
+        }else{
+          this.search(col, row);
+        }
       });
+
+      this.draw();
+
     }
 
-
-      //タイルのクリックアクション
-      search(col, row){
-        if(this.tiles[row][col] === 2){
-          console.log('ここは２');
-          // this.isMissed = true;
-        }else{
-          let t = 0;
-
-          if(row === 0 && col === 0){//左上
-            for(let a = row; a <= row +1; a++) {
-              for(let b = col; b <= col +1; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(row === 0 && col === this.tileCol -1){//右上
-            for(let a = row; a <= row +1; a++) {
-              for(let b = col -1; b <= col; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(row === this.tileRow -1 && col === 0){//左下
-            for(let a = row -1; a <= row; a++) {
-              for(let b = col; b <= col +1; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(row === this.tileRow -1 && col === this.tileCol -1){//右下
-            for(let a = row -1; a <= row; a++) {
-              for(let b = col -1; b <= col; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(row === 0){//上
-            for(let a = row; a <= row +1; a++) {
-              for(let b = col -1; b <= col +1; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(row === this.tileRow -1){//下
-            for(let a = row -1; a <= row; a++) {
-              for(let b = col -1; b <= col +1; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(col === 0){//左
-            for(let a = row -1; a <= row +1; a++) {
-              for(let b = col; b <= col +1; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
-          }
-          else if(col === this.tileCol -1){//右
-            for(let a = row -1; a <= row +1; a++) {
-              for(let b = col -1; b <= col; b++) {
-                t += this.tiles[a][b] -1;
-              }
-            }
+    // 0からmax-1までの整数を返す
+    getRandomInt(max) {
+      // ランダムな配列
+      return Math.floor(Math.random() * max);
+    }
+    getTiles(tileRow,tileCol,max){
+      for (let i = 0; i < tileRow; i++) {
+        this.tile_array = [];
+        for (let i = 0; i < tileCol; i++) {
+          if(this.getRandomInt(max) === 0){
+            this.tile_array.push(2);
           }
           else{
-            for(let a = row -1; a <= row +1; a++) {
-              for(let b = col -1; b <= col +1; b++) {
-                t += this.tiles[a][b] -1;
-              }
+            this.tile_array.push(1);
+          }
+        }
+        this.tiles.push(this.tile_array);
+      }
+    }
+
+    //タイルのクリックアクション
+    search(col, row){
+      let t = 0;
+
+      if(this.tiles[row][col] === 2){
+        console.log('ここは２');
+        t = '💣';
+        this.searchResult(t,col, row);
+        this.isMissed = true;
+        this.game.drawGameOver();
+      }else{
+        if(row === 0 && col === 0){//左上
+          for(let a = row; a <= row +1; a++) {
+            for(let b = col; b <= col +1; b++) {
+              t += this.tiles[a][b] -1;
             }
           }
-
-          console.log(t);
-          this.searchResult(t,col, row);
         }
+        else if(row === 0 && col === this.tileCol -1){//右上
+          for(let a = row; a <= row +1; a++) {
+            for(let b = col -1; b <= col; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else if(row === this.tileRow -1 && col === 0){//左下
+          for(let a = row -1; a <= row; a++) {
+            for(let b = col; b <= col +1; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else if(row === this.tileRow -1 && col === this.tileCol -1){//右下
+          for(let a = row -1; a <= row; a++) {
+            for(let b = col -1; b <= col; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else if(row === 0){//上
+          for(let a = row; a <= row +1; a++) {
+            for(let b = col -1; b <= col +1; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else if(row === this.tileRow -1){//下
+          for(let a = row -1; a <= row; a++) {
+            for(let b = col -1; b <= col +1; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else if(col === 0){//左
+          for(let a = row -1; a <= row +1; a++) {
+            for(let b = col; b <= col +1; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else if(col === this.tileCol -1){//右
+          for(let a = row -1; a <= row +1; a++) {
+            for(let b = col -1; b <= col; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+        else{
+          for(let a = row -1; a <= row +1; a++) {
+            for(let b = col -1; b <= col +1; b++) {
+              t += this.tiles[a][b] -1;
+            }
+          }
+        }
+
+        this.searchResult(t,col, row);
+
+        // drawGameclear()
       }
+    }
 
-      searchResult(t,col, row){
-        // this.ctx.font      = '28px "Arial Black"';
-        this.ctx.fillStyle = '#fdfdfd';
-        this.ctx.fillText(t, 50, 150);
-      }
-
-    update(){
-
+    searchResult(t,col, row){
+      this.ctx.font      = '15px "Arial Black"';
+      this.ctx.fillStyle = '#fdfdfd';
+      this.ctx.fillText(
+        t,
+        this.tileSize * col + this.tileSize/2-5,
+        this.tileSize * row + this.tileSize/2+5
+      );
     }
 
     //ブロック初期配置用２重配列を回し座標を[row][col]で取得
     draw(){
       for (let row = 0; row < this.tileRow; row++){
         for (let col = 0; col < this.tileCol; col++){
-          if(this.tiles[row][col]){
-            this.data.push({
-              x : this.x + col * this.tileSize, 
-              y : this.y + row * this.tileSize, 
-              w : this.tileSize, 
-              h : this.tileSize,
-              b : this.tiles[row][col]//1:タイル　2：爆弾
-            })
-          }
+
+          this.ctx.strokeStyle = '#fdfdfd';
+          this.ctx.strokeRect(
+            col * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            this.tileSize
+          );
         }
       }
-      this.drawTiles(this.data);
-
-    }
-
-    drawTiles(data){
-      data.forEach(brick => {
-        // this.ctx.fillStyle = '#fdfdfd';
-        this.ctx.strokeStyle = '#fdfdfd';
-        // this.ctx.fillRect(
-        this.ctx.strokeRect(
-          brick.x, 
-          brick.y, 
-          brick.w, 
-          brick.h
-        );
-      });
     }
 
     //失敗処理
@@ -184,29 +183,10 @@
     constructor(canvas) {
       this.canvas     = canvas;
       this.ctx        = this.canvas.getContext('2d');
-      this.tiles      = new Tiles(this.canvas);
+      this.tiles      = new Tiles(this.canvas,this);
       // this.bomb       = new Bomb(this.canvas);
       this.isGameOver = false;
-      this.loop();
-    }
-    loop(){
-      if (this.isGameOver){
-          return;
-      }
-      // this.update();
       this.draw();
-
-      //再帰的処理
-      // requestAnimationFrame(() => {
-      //   this.loop();
-      // });
-    }
-
-    update(){
-      if (this.tiles.getMissedStatus()) {
-        this.isGameOver = true;
-      }
-      this.tiles.update();
     }
 
     draw(){
@@ -223,6 +203,13 @@
       this.ctx.font      = '28px "Arial Black"';
       this.ctx.fillStyle = 'tomato';
       this.ctx.fillText('GAME OVER', 50, 150);
+    }
+    //クリア画面
+    drawGameclear() {
+      this.ctx.font      = '28px "Arial Black"';
+      this.ctx.fillStyle = 'tomato';
+      this.ctx.fillText('GAME CLEAR', 50, 150);
+
     }
 
   }
