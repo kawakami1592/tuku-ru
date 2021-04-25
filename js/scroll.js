93dba5460d64f7e527a8ac5d711a9a9507acfa54
kawakami1592 <m.kawakami157@icloud.com>
2021-04-25 22:18:53 +0900

    sukuro-ru

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+
+const targetElemnt = document.querySelectorAll(".animationTarget");
+
+document.addEventListener("scroll",function(){
+  for(let i = 0; i < targetElemnt.length; i++){
+    //ブラウザ(画面)の上からの要素の上端の距離＋要素の高さ*0.6
+    const getElemntDistance = targetElemnt[i].getBoundingClientRect().top + targetElemnt[i].clientHeight * 0.6;
+    
+    if(getElemntDistance < window.innerHeight){
+      targetElemnt[i].classList.add("show");
+
+    }
+  }
+})
